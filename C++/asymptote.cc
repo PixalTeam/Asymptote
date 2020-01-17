85af769d6890db9fddcbba31fd0358ceb3f2748a
Kellis <vikiclashonyt@gmail.com>
2020-01-17 22:53:44 +0100

    For C++
    C++ is a general-purpose programming language.

--- C++/asymptote.cc
+++ C++/asymptote.cc
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main() {
-    double p, n, u;
+    static double p, u, n;
     cout << "\"p\" value: ";
     cin >> p;
     u = 3;
     while (abs(u - 2) >= pow(10, -p)) {
-        n = n + 1;
-        u = (2*n + 3)/(n + 1);
+        n += 1;
+        u = (2 * n + 3) / (n + 1);
     }
     
     std::cout << n << std::endl;
